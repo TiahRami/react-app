df74207c63fb04c09f2196b2c785991f643feb1b
Tiah Rami <minotia001@gmail.com>
2022-07-11 23:52:49 +0100

    added styles to the note app

--- forms/src/App.js
+++ forms/src/App.js
@@ -1,11 +1,23 @@
 import { useState, useEffect } from 'react'
 import Note from './component/Note'
-import noteService from ".services/notes"
+import noteService from "./services/notes"
+
+const Notification = ({message}) => {
+  if(message === null) {
+    return null
+  }
+  return (
+    <div className='error'>
+      {message}
+    </div>
+  )
+}
 
 const App = () => {
   const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
+  const [errorMessage, setErrorMessage] = useState("Some error happened...")
 
   useEffect(() => {
     noteService
@@ -35,17 +47,23 @@ const App = () => {
     setNewNote(event.target.value)
   }
 
+
   const toggleImportanceOf = (id) => {
-    const note = note.find(n => n.id === id)
-    const changedNote = {...note, important: !note.important}
+    const note = notes.find(n => n.id === id)
+    const changedNote = { ...note, important: !note.important }
+
     noteService
-      .update(id, changedNote)
-      .then(returnedNote => {
+      .update(changedNote).then(returnedNote => {
         setNotes(notes.map(note => note.id !== id ? note : returnedNote))
       })
-      .catch( () => {
-        alert(`The note "${note.content}" was already deleted form the server`)
-        setNotes(note.filter(n => n.id !== id))
+      .catch(error => {
+        setErrorMessage(
+          `Note '${note.content}' was already removed from server`
+        )
+        setTimeout(() => {
+          setErrorMessage(null)
+        }, 5000)
+        setNotes(notes.filter(n => n.id !== id))
       })
   }
 
@@ -56,6 +74,7 @@ const App = () => {
   return (
     <div>
       <h1>Notes</h1>
+      <Notification message={errorMessage}/>
       <div>
         <button onClick={() => setShowAll(!showAll)}>
           show {showAll ? 'important' : 'all'}
